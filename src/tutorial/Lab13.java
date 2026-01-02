513ab1c11a0e3f71cc55a826e909bb8bece014d4
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 11:08:38 +0530

    Create Lab13

---
+++ src/tutorial/Lab13.java
@@ -0,0 +1,11 @@
+package tutorial;
+
+public class Lab13 {
+	public static void main(String[]args) {
+		String str1="JLC",str2="SRI";
+		System.out.println(str1);
+		System.out.println(str2);
+	}
+	
+
+}
