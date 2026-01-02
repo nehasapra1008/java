fd77853dae38f27ae761efe6d61cf99f7d0080a2
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 10:52:32 +0530

    Create Lab11

---
+++ src/tutorial/Lab11.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class Lab11 {
+	public static void main(String[]args) {
+		int a,b,c;
+		a=b=c=99;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		
+	}
+
+}
