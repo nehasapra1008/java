1e6a6ee48bc9c7449dccc2369706c52b0dbad1cf
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 10:34:41 +0530

    Create Lab9

---
+++ src/tutorial/Lab9.java
@@ -0,0 +1,15 @@
+package tutorial;
+
+public class Lab9 {
+	public static void main(String[]args) {
+		int a=99;
+		boolean b=true;
+		double d=999.99;
+		String str="srinivas";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(str);
+	}
+
+}
