afa3528c3c3187e2a70bd4fcc2166f45e919e8ad
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 17:26:41 +0530

    Create Lab17

---
+++ src/tutorial/Lab17.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+public class Lab17 {
+	public static void main(String[]args) {
+		int b,c;
+		int a=b=c=99;
+		System.out.println(a);
+		System.out.println(a);
+		System.out.println(c);
+	}
+
+}
