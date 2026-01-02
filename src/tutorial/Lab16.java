260469824b2a20de41bd5b0d1ea1b74e16e863fb
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 17:10:42 +0530

    Create Lab16

---
+++ src/tutorial/Lab16.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+public class Lab16 {
+	public static void main (String[]args) {
+		int=a=b=c=88;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
