d47aa55a4b2d31c0c428141ccfaf3436df370e56
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 11:04:08 +0530

    Create Lab12

---
+++ src/tutorial/Lab12.java
@@ -0,0 +1,15 @@
+package tutorial;
+
+public class Lab12 {
+	public static void main(String[]args) {
+		String str1,str2,str3;
+		str1=str2=str3="JLC";
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		
+	}
+	
+
+}
