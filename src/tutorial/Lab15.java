123c2f77d1eea26903297916cd337dcea299bc49
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 16:53:14 +0530

    Create Lab15

---
+++ src/tutorial/Lab15.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+public class Lab15 {
+	public static void main(String[]args) {
+		String str1,str2;
+		String str3= str1=str2="JLC";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+
+}
