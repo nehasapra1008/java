858456315fce1f78c4cefe861071267ca027f299
Neha Sapra <nehasapra1995@gmail.com>
2026-01-02 17:32:19 +0530

    Create Lab18

---
+++ src/tutorial/Lab18.java
@@ -0,0 +1,13 @@
+package tutorial;
+
+public class Lab18 {
+	public static void main(String[]args) {
+		int a,b;
+		int c=10,a=20,b=30;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
